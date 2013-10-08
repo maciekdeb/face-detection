8ed4c6f0e2fc386a1427c86c07e0c8b0adf7b36f
noldi <maciekdeb@gmail.com>
2013-10-08 18:16:14 +0200

    Main class created

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: maciek
+ * Date: 08.10.13
+ * Time: 18:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
