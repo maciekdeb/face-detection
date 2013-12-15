c80b8df67f076541d7621b9a9b606890dbc898c7
noldi <maciekdeb@gmail.com>
2013-12-16 00:19:30 +0100

    integral image

--- src/main/java/pl/lodz/p/ics/model/IntegralImage.java
+++ src/main/java/pl/lodz/p/ics/model/IntegralImage.java
@@ -9,20 +9,48 @@ import java.awt.image.BufferedImage;
  */
 public class IntegralImage {
 
-    private BufferedImage bufferedImage;
     private double[][] integralImage;
 
     public IntegralImage(BufferedImage bufferedImage) {
-        this.bufferedImage = bufferedImage;
+
+        this.integralImage = new double[bufferedImage.getHeight()][bufferedImage.getWidth()];
 
         for (int x = 0; x < bufferedImage.getWidth(); x++) {
             for (int y = 0; y < bufferedImage.getHeight(); y++) {
-
-                //TODO
-
+                for (int i = 0; i <= x; i++) {
+                    for (int j = 0; j <= y; j++) {
+                        //TODO
+                        integralImage[y][x] += bufferedImage.getRGB(i, j);
+                    }
+                }
             }
         }
     }
 
+    public double getRectangleValue(Point topLeft, Point bottomRight) {
 
+        if (topLeft.getX() > bottomRight.getX() || topLeft.getY() > bottomRight.getY()) {
+            throw new IllegalArgumentException("Rectangle not valid");
+        }
+
+        double result = integralImage[bottomRight.getY()][bottomRight.getX()];
+
+        if (topLeft.getX() > 0) {
+            result -= integralImage[bottomRight.getY()][topLeft.getX() - 1];
+        }
+
+        if (topLeft.getY() > 0) {
+            result -= integralImage[topLeft.getY() - 1][bottomRight.getX()];
+        }
+
+        if (topLeft.getX() > 0 && topLeft.getY() > 0) {
+            result += integralImage[topLeft.getY() - 1][topLeft.getX() - 1];
+        }
+
+        return result;
+    }
+
+    public double[][] getIntegralImage() {
+        return integralImage;
+    }
 }
---
+++ src/test/java/IntegralImageTest.java
@@ -0,0 +1,57 @@
+import junit.framework.Assert;
+import org.junit.Test;
+import pl.lodz.p.ics.model.IntegralImage;
+import pl.lodz.p.ics.model.Point;
+
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: maciek
+ * Date: 15.12.13
+ * Time: 22:14
+ */
+public class IntegralImageTest {
+
+    int a[][] = new int[][]{
+            new int[]{1, 1, 1, 1, 1, 1, 1},
+            new int[]{2, 2, 2, 2, 2, 2, 2},
+            new int[]{3, 3, 3, 3, 3, 3, 3},
+            new int[]{4, 4, 4, 4, 4, 4, 4}
+    };
+
+    @Test
+    public void testPasses() {
+        BufferedImage image = new BufferedImage(7, 4, BufferedImage.TYPE_4BYTE_ABGR);
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                image.setRGB(j, i, a[i][j]);
+            }
+        }
+
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                System.out.print(image.getRGB(i, j) + " ");
+            }
+            System.out.print("\n");
+        }
+
+        IntegralImage integralImage = new IntegralImage(image);
+
+        for (int i = 0; i < integralImage.getIntegralImage().length; i++) {
+            for (int j = 0; j < integralImage.getIntegralImage()[i].length; j++) {
+                System.out.print(integralImage.getIntegralImage()[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
+
+        Assert.assertEquals(1.0, integralImage.getRectangleValue(new Point(0, 0), new Point(0, 0)));
+        Assert.assertEquals(6.0, integralImage.getRectangleValue(new Point(0, 0), new Point(1, 1)));
+        Assert.assertEquals(9.0, integralImage.getRectangleValue(new Point(1, 2), new Point(3, 2)));
+        Assert.assertEquals(10.0, integralImage.getRectangleValue(new Point(1, 1), new Point(2, 2)));
+
+    }
+
+}
